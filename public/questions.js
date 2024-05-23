36903ace7b9dd19d34167c32dfc111b4845e8623
Laise1209 <laise.almeida@sptech.school>
2024-05-23 15:20:28 -0300

    O quiz está funcionando, falta a integração dele com o banco

---
+++ public/questions.js
@@ -0,0 +1,83 @@
+
+    export default[
+    {
+        question: "Em que ano a banda foi montada?",
+        answers: [
+            {option: "1990", correct: false},
+            {option: "1981", correct: true},
+            {option: "1985", correct: false}
+        ]
+    },
+    {
+        question: "Antes de ser Metallica, qual foi o primeiro nome da banda?",
+        answers: [
+            {option: "Metal Mania", correct: true},
+            {option: "The Young Metal Attack", correct: false},
+            {option: "Skillet", correct: false}
+        ]
+    },
+    {
+        question: "Quem são os fundadores do Metallica?",
+        answers: [
+            {option: "James Hetfield e Robert Trujillo", correct: false},
+            {option: "Dave Mustaine e Lars Ulrich", correct: false},
+            {option: "James Hetfield e Lars Ulrich", correct: true}
+        ]
+    },
+    {
+        question: "Por que Dave Mustaine foi demitido do Metallica?",
+        answers: [
+            {option: "Porque ele estava monstando outra banda, o Megadeth", correct: false},
+            {option: "Por causa do abuso de substâncias e a inconsistência musical", correct: true},
+            {option: "Pois Dave a Lars brigaram", correct: false}
+        ]
+    },
+    {
+        question: "Quem era Ron McGovney?",
+        answers: [
+            {option: "O primeiro baixista do Metallica", correct: true},
+            {option: "O ex-produtor da banda", correct: false},
+            {option: "O primeiro guitarrista do Metallica", correct: false}
+        ]
+    },
+    {
+        question: "Kill'em all foi o primeiro disco lançado pela banda, mas qual era a ideia original do nome?",
+        answers: [
+            {option: "A cry from the edger", correct: false},
+            {option: "Metal up your ass", correct: true},
+            {option: "Evisceration plague", correct: false}
+        ]
+    },
+    {
+        question: "Quem foi o último integrante a entrar para banda?",
+        answers: [
+            {option: "Kirk Hammet", correct: false},
+            {option: "Cliff Burton", correct: false},
+            {option: "Robert Trujillo", correct: true}
+        ]
+    },
+    {
+        question: "A banda em 1991 mudou o público alvo, saindo do thrash metal e indo para o heavy metal, lançando o álbum:",
+        answers: [
+            {option: "... And Justice For All", correct: false},
+            {option: "Metallica", correct: true},
+            {option: "Reload", correct: false}
+        ]
+    },
+    {
+        question: "Quais foram os piores álbuns da banda?",
+        answers: [
+            {option: "Death Magnetic e The four horseman", correct: false},
+            {option: "Load e Hardwire to self-destruct", correct: false},
+            {option: "Lulu e St Anger", correct: true}
+        ]
+    },
+    {
+        question: "O que aconteceu com o baixista Cliff Burton para sair do Metallica?",
+        answers: [
+            {option: "Faleceu em um acidente", correct: true},
+            {option: "Saiu da banda por motivos pessoais", correct: false},
+            {option: "Foi expulso", correct: false}
+        ]
+    }
+    ]
